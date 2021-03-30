342b9a5a386b53a53f5d33f6d42b890bfbfa6ae5
Rafael <rafaelemefsp@gmail.com>
2021-03-30 17:40:28 -0300

    Configurações inicias do projeto

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require ("express")
 const app = express()
+var bodyParser = require("body-parser")
+var cookieParser = require("cookie-parser")
+var path = require("path") 
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine", "ejs")
+
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get ('/', function(req,res){
 res.send ("Olá Rafael")
-})
-
-app.get('/msg', function(req,res){
-    res.send ("Essa mensagem é automática")
-})
-
-app.get('/sobre', function(req,res){
-    res.send ("Essa página esta sendo desenvolvida por Rafael")
-}) 
-app.get('/layout', function(req,res){
-    res.send ("Esse é o novo layout")
 })
 
 
